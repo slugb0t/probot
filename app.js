57dc299ca166e4fa025033ba99e6b5b65da418c0
slugb0t <wheresdorian@gmail.com>
2023-01-23 14:07:17 -0800

    test vercel deployment

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-/**
- * @param {import('probot').Probot} app
- */
-module.exports = (app) => {
-  app.log("Yay! The app was loaded!")
+module.exports = (app, { getRouter }) => {
+  // Your code here
 
-  app.onAny(async (context) => {
-    app.log.info({ event: context.name, action: context.payload });
-    context.webhook({
-      event: context.name,
-      data: {
-        message: context.payload
-      }
-    })
+  const router = getRouter("/");
+
+  router.post("/api/github/webhooks", (req, res) => {
+    // Handle webhook event
+    console.log(req.body);
+    res.status(200).end();
   });
 
+  app.route(router);
+
+  app.onAny(async (context) => {
+    // app.log({event: context.name, action: context.payload[0]})
+    app.log.info({ event: context.name, action: context.payload });
+    await axios.post(
+      "https://probot-slugb0t.vercel.app/api/github/webhooks",
+      context.payload
+    );
+  });
+
+  app.log.info("Yay, the app was loaded!");
 };
